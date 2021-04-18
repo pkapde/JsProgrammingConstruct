6561a94bb7df1f4c2e6a26531ad5a7743ecfc103
prashant <pkapde308@gmail.com>
2021-04-18 21:25:48 +0530

    Added:code to get number between 1 to 6

--- Sequence_problems.js
+++ Sequence_problems.js
@@ -1,4 +1,7 @@
 // 1) Use REPL – Random Function Math.floor(Math.random() * 10); to get Single Digit.
 let singleDigit=Math.floor(Math.random()*10);
-console.log("Random function to get single digit :"+singleDigit);
+console.log("single digit :"+singleDigit);
 
+// 2) Use REPL – Use Random to get Dice Number between 1 to 6
+let diceNumber=(Math.floor(Math.random()*10)%6)+1;
+console.log("Random number :"+diceNumber);
