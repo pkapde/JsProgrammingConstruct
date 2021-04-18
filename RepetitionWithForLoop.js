e8c478ef8d8b8100da14a83d7953622cfa1cb65e
prashant <pkapde308@gmail.com>
2021-04-19 01:55:55 +0530

    Added:code that takes a command-line argument n and prints the nth harmonic number

--- RepetitionWithForLoop.js
+++ RepetitionWithForLoop.js
@@ -8,3 +8,14 @@ if (n1 >= 0 && Number.isInteger(n1)) {
         console.log("2^" + i + " = " + Math.pow(2, i));
     }
 } else console.log("Enter only non-negative integers.");
+
+// 2. Write a program that takes a command-line argument n and prints the nth harmonic number.
+console.log("\nTo get nth harmonic number:");
+const n2 = Number(prompt("Enter a positive integer (i.e. 'n' value): "));
+if (n2 >= 1 && Number.isInteger(n2)) {
+    let ans = 0;
+    for (let i = 1; i <= n2; i++) {
+        ans += Math.pow(i, -1);
+    }
+    console.log("The harmonic number is: " + ans);
+} else console.log("Enter only positive integers.");
