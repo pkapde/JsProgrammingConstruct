0388e9a143d2341e88cf014059b09917fea7e6af
prashant <pkapde308@gmail.com>
2021-04-19 01:48:16 +0530

    Added:code to Read a number and display the week-day (Sunday, Monday,…)

--- SelectionWithCase.js
+++ SelectionWithCase.js
@@ -19,3 +19,18 @@ switch (num) {
 }
 console.log("Your entered number in word is: " + numWord);
 
+// 2. Read a number and display the week-day (Sunday, Monday,…)
+console.log("\nEnter a digit to get the week-day:");
+const weekNum = Number(prompt("week number: "));
+let weekDay;
+switch (weekNum) {
+    case 0: weekDay = "Sunday"; break;
+    case 1: weekDay = "Monday"; break;
+    case 2: weekDay = "Tuesday"; break;
+    case 3: weekDay = "Wednesday"; break;
+    case 4: weekDay = "Thursday"; break;
+    case 5: weekDay = "Friday"; break;
+    case 6: weekDay = "Saturday"; break;
+    default: console.log("Invalid user input.");
+}
+console.log("Your entered number equals the weekday: " + weekDay);
