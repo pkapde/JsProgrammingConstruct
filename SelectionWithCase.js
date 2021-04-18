a674cb3b91bc38a2851c8eef35f1c375301ad429
prashant <pkapde308@gmail.com>
2021-04-19 01:49:40 +0530

    Added:code to Read a number 1, 10, 100, 1000, etc and display unit, ten, hundred

--- SelectionWithCase.js
+++ SelectionWithCase.js
@@ -34,3 +34,22 @@ switch (weekNum) {
     default: console.log("Invalid user input.");
 }
 console.log("Your entered number equals the weekday: " + weekDay);
+
+// 3. Read a number 1, 10, 100, 1000, etc and display unit, ten, hundred,…
+console.log("\nEnter a number of this form 1, 10, 100, 1000, etc:");
+const number = Number(prompt("number: "));
+let numValue;
+switch (number) {
+    case 1: numValue = "Unit"; break;
+    case 10: numValue = "Ten"; break;
+    case 100: numValue = "Hundred"; break;
+    case 1000: numValue = "Thousand"; break;
+    case 10000: numValue = "Ten Thousand"; break;
+    case 100000: numValue = "Lakh"; break;
+    case 1000000: numValue = "Ten Lakh"; break;
+    case 10000000: numValue = "Crore"; break;
+    case 100000000: numValue = "Ten Crore"; break;
+    case 1000000000: numValue = "Hundred Crore"; break;
+    default: console.log("Invalid user input.");
+}
+console.log("Your entered number is: " + numValue);
