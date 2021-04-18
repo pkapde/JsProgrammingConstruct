6a034b810ef9abef6a5e8acb63cbd816dd63c8b6
prashant <pkapde308@gmail.com>
2021-04-18 23:41:11 +0530

    Added:code to unit conversion

--- Sequence_problems.js
+++ Sequence_problems.js
@@ -22,3 +22,17 @@ const num5 = Number(prompt("num5: "));
 let sum = num1 + num2 + num3 + num4 + num5;
 console.log("Sum of 5 random 2-digit values: " + sum);
 console.log("Average of 5 random 2-digit values: " + (sum / 5));
+
+//Use Script & Debug – Unit Conversion
+//a. 1ft = 12 in then 42 in = ? ft
+let foot, inch = 42;
+foot = inch / 12;
+console.log(`\nInches to feet conversion: 42 inches = ${foot} feet`);
+//b. Rectangular Plot of 60 feet x 40 feet in meters
+let length_in_feet = 60, breadth_in_feet = 40;
+let length_in_m = length_in_feet / 3.2808, breadth_in_m = breadth_in_feet / 3.2808;
+console.log(`\nA rectangular plot of 60 feet x 40 feet converted to meters gives a rectangular plot of ${length_in_m} m x ${breadth_in_m} m`);
+//c. Calculate area of 25 such plots in acres
+let area_in_sq_m = length_in_m * breadth_in_m;
+let area_in_acres = area_in_sq_m * 0.00024711;
+console.log("\nArea of 25 rectangular plots in acres: " + (25 * area_in_acres));
