59bbcf7dfcf9599297117718b0e2ed543df4f9b6
prashant <pkapde308@gmail.com>
2021-04-18 21:24:42 +0530

    Added:code to get single digit using random function

---
+++ Sequence_problems.js
@@ -0,0 +1,4 @@
+// 1) Use REPL – Random Function Math.floor(Math.random() * 10); to get Single Digit.
+let singleDigit=Math.floor(Math.random()*10);
+console.log("Random function to get single digit :"+singleDigit);
+
