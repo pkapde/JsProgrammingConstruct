be60540efdffe7d92ee200339a7cd721380f26b6
prashant <pkapde308@gmail.com>
2021-04-18 21:27:12 +0530

    Added:code to add two random numbers

--- Sequence_problems.js
+++ Sequence_problems.js
@@ -5,3 +5,8 @@ console.log("single digit :"+singleDigit);
 // 2) Use REPL – Use Random to get Dice Number between 1 to 6
 let diceNumber=(Math.floor(Math.random()*10)%6)+1;
 console.log("Random number :"+diceNumber);
+
+// 3) Use REPL – Add two Random Dice Number and Print the Result
+let diceNumber1=(Math.floor(Math.random()*10)%6)+1;
+let diceNumber2=(Math.floor(Math.random()*10)%6)+1;
+console.log("Addition of two random numbers :"+(diceNumber+diceNumber2));
