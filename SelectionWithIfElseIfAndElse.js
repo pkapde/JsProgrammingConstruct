290ed189a8d87cecbac800e3921090fcd7a87f69
prashant <pkapde308@gmail.com>
2021-04-19 01:43:56 +0530

    Added:code to Read a number 1, 10, 100, 1000, etc and display unit, ten, hundred,…

--- SelectionWithIfElseIfAndElse.js
+++ SelectionWithIfElseIfAndElse.js
@@ -34,3 +34,20 @@ if (weekNum >= 0 && weekNum <= 6) {
     console.log("Your entered number equals the weekday: " + weekDay);
 }
 else console.log("Invalid user input.");
+
+// 3. Read a number 1, 10, 100, 1000, etc and display unit, ten, hundred,…
+console.log("\nEnter a number of this form 1, 10, 100, 1000, etc:");
+const number = Number(prompt("number: "));
+let numValue;
+if (number == 1) numValue = "Unit";
+else if (number == 10) numValue = "Ten";
+else if (number == 100) numValue = "Hundred";
+else if (number == 1000) numValue = "Thousand";
+else if (number == 10000) numValue = "Ten Thousand";
+else if (number == 100000) numValue = "Lakh";
+else if (number == 1000000) numValue = "Ten Lakh";
+else if (number == 10000000) numValue = "Crore";
+else if (number == 100000000) numValue = "Ten Crore";
+else if (number == 1000000000) numValue = "Hundred Crore";
+else console.log("Invalid user input.");
+console.log("Your entered number is: " + numValue);
