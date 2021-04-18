91b8ef8101305cb1240d18584f05439813ee194d
prashant <pkapde308@gmail.com>
2021-04-19 01:43:10 +0530

    Added:code to Read a number and display the week day (Sunday, Monday..)

--- SelectionWithIfElseIfAndElse.js
+++ SelectionWithIfElseIfAndElse.js
@@ -18,3 +18,19 @@ if (num >= 0 && num <= 9) {
     console.log("Your entered number in word is: " + numWord);
 }
 else console.log("Invalid user input.");
+
+// 2. Read a number and display the week day (Sunday, Monday,…).
+console.log("\nEnter a digit to get the week-day:");
+const weekNum = Number(prompt("week number: "));
+let weekDay;
+if (weekNum >= 0 && weekNum <= 6) {
+    if (weekNum == 0) weekDay = "Sunday";
+    else if (weekNum == 1) weekDay = "Monday";
+    else if (weekNum == 2) weekDay = "Tuesday";
+    else if (weekNum == 3) weekDay = "Wednesday";
+    else if (weekNum == 4) weekDay = "Thursday";
+    else if (weekNum == 5) weekDay = "Friday";
+    else if (weekNum == 6) weekDay = "Saturday";
+    console.log("Your entered number equals the weekday: " + weekDay);
+}
+else console.log("Invalid user input.");
