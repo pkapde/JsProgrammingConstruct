9f5f5afce21460bad2b409d6fde5e35790e2554f
prashant <pkapde308@gmail.com>
2021-04-19 00:04:49 +0530

    Added:code to check leap year

--- SelectionWithIfElse.js
+++ SelectionWithIfElse.js
@@ -29,3 +29,13 @@ if ((month === "March" && day >= 21 && day <= 31) || (month === "April" && day >
     check = true;
 else check = false;
 console.log("Day of Month is between March 20 and June 20: " + check);
+
+// 3. Write a program that takes a year as input and outputs the year is a Leap Year or not a Leap Year.
+// A Leap Year checks for 4-digit Number, divisible by 4 and not 100 unless divisible by 400.
+console.log("\nEnter a 4-digt year to check if Leap Year or not:");
+const year = Number(prompt("Enter year: "));
+if (year >= 1000 && year <= 9999) {
+    if ((year % 4 === 0 && year % 100 != 0) || (year % 400 === 0)) console.log("It's a leap year.");
+    else console.log("It's a non-leap year.");
+}
+else console.log("User-input is not a 4-digit year.");
