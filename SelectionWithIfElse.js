e1ac99d2ce6e5c444d1abbe35ea32534c29b245c
prashant <pkapde308@gmail.com>
2021-04-19 00:03:57 +0530

    Added:code to if  day of month is between March 20 and June 20, false otherwise

--- SelectionWithIfElse.js
+++ SelectionWithIfElse.js
@@ -18,3 +18,14 @@ else if(number == 5)
 else
     console.log("Wrong input.");
 }
+
+// 2. Write a program that takes day and month from the command line and prints true if  day of month is between March 20 and June 20, false otherwise.
+console.log("\nTo check if day of month given as input by user is between March 20 and June 20:");
+const day = prompt("Enter any day of the month: ");
+const month = prompt("Enter any month of the year: ");
+let check;
+if ((month === "March" && day >= 21 && day <= 31) || (month === "April" && day >= 1 && day <= 30)
+    || (month === "May" && day >= 1 && day <= 31) || (month === "June" && day >= 1 && day <= 19))
+    check = true;
+else check = false;
+console.log("Day of Month is between March 20 and June 20: " + check);
