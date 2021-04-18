d7a2306a118b03d9de5b83503f0c47ba26656177
prashant <pkapde308@gmail.com>
2021-04-18 21:30:13 +0530

    Added:code to read 5 random 2 digit values to find sum and average

--- Sequence_problems.js
+++ Sequence_problems.js
@@ -10,3 +10,15 @@ console.log("Random number :"+diceNumber);
 let diceNumber1=(Math.floor(Math.random()*10)%6)+1;
 let diceNumber2=(Math.floor(Math.random()*10)%6)+1;
 console.log("Addition of two random numbers :"+(diceNumber+diceNumber2));
+
+// 4. Use Script & Debug – Write a program that reads 5 random 2-digit values, then find their sum and the average.
+const prompt = require('prompt-sync')({ sigint: true });
+console.log("\nEnter 5 random 2-digit numbers to calculate their sum and average:");
+const num1 = Number(prompt("num1: "));
+const num2 = Number(prompt("num2: "));
+const num3 = Number(prompt("num3: "));
+const num4 = Number(prompt("num4: "));
+const num5 = Number(prompt("num5: "));
+let sum = num1 + num2 + num3 + num4 + num5;
+console.log("Sum of 5 random 2-digit values: " + sum);
+console.log("Average of 5 random 2-digit values: " + (sum / 5));
