9fdcd8ba019d4c4c29de3af2b728f153b77c910b
prashant <pkapde308@gmail.com>
2021-04-19 01:57:48 +0530

    Added:code to take a range of numbers as input and output the prime-numbers in that range

--- RepetitionWithForLoop.js
+++ RepetitionWithForLoop.js
@@ -35,3 +35,22 @@ if (n3 >= 1 && Number.isInteger(n3)) {
     if (isPrime) console.log(n3 + " is a prime number.");
     else console.log(n3 + " is a non-prime number.");
 } else console.log("Enter only positive integers.");
+
+// 4. Extend the program to take a range of numbers as input and output the prime-numbers in that range.
+console.log("\nTo determine all prime numbers in a user-given number range:\nEnter only positive integer number-range:");
+const low = Number(prompt("Enter lower limit: "));
+const high = Number(prompt("Enter higher limit: "));
+if (low <= high && low > 0 && Number.isInteger(low) && Number.isInteger(high)) {
+    console.log("Prime numbers in this range:");
+    for (let i = low; i <= high; i++) {
+        let isPrime = true;
+        if (i == 1) isPrime = false;
+        for (let j = 2; j <= i / 2; j++) {
+            if (i % j == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) console.log(i);
+    }
+} else console.log("Invalid user number-range input.");
