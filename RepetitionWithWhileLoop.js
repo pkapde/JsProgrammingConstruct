ce1bc905d21fcbc4be6c34b41792fba62a969395
prashant <pkapde308@gmail.com>
2021-04-20 01:28:30 +0530

    Added:code to find magic number

--- RepetitionWithWhileLoop.js
+++ RepetitionWithWhileLoop.js
@@ -10,3 +10,28 @@ const prompt = require('prompt-sync')();
         i++;
     }
 }
+
+// 2) Find the Magic Number
+// a. Ask the user to think of a number n between 1 to 100
+// b. Then check with the user if the number is less then n/2 or greater
+// c. Repeat till the Magic Number is reached..
+{
+    let minNumber=1
+    let maxNumber=100
+    let midNumber=0
+    while ( minNumber <= maxNumber ){
+        midNumber=parseInt( (minNumber+maxNumber)/2 );
+        console.log("Your number is lesser(l)/greater(g)/equals(e) : "+midNumber);
+        input=prompt("Enter your option :");
+        if(input == 'l')
+            maxNumber=midNumber;
+        else if (input == 'g')
+            minNumber=midNumber;
+        else if (input == 'e'){
+            console.log("Your magic number is :"+midNumber);
+            break;
+        }
+        else
+            console.log("Wrong input")
+    } 
+}
