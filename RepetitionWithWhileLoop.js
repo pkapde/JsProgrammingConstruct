c25501428085497058dbea1fb9f17260fe69cea7
prashant <pkapde308@gmail.com>
2021-04-20 01:29:34 +0530

    Added:code to head or tails

--- RepetitionWithWhileLoop.js
+++ RepetitionWithWhileLoop.js
@@ -35,3 +35,18 @@ const prompt = require('prompt-sync')();
             console.log("Wrong input")
     } 
 }
+
+// 3) Extend the Flip Coin problem till either Heads or Tails wins 11 times.
+{
+    let noOfTails=0
+    let noOfHeads=0
+    while(noOfHeads < 11 && noOfTails < 11){
+        let toss=Math.floor(Math.random()*10)%2;
+        if(toss == 0)
+            noOfHeads++;
+        else
+            noOfTails++;
+    }
+    console.log("No of head wins are : " +noOfHeads+" No of tail wins are : " +noOfTails);
+}
+
